1e127666c44e1a04bb224064c00d75b4f06b2011
Tomasz Budzyński <tomasz.budzynski.programista@gmail.com>
2020-05-06 12:21:31 +0200

    Add user menu

--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
+#include <stdlib.h>
+#include <limits>   //potrrzebne do okreœlenia maksymalnej dlugosci strumienia wejscia cin - podczas czysczenia strumienia
+#include <ios>      // potrzebne do <streamsize>
+#include <windows.h>
 #include <fstream>
+#include <vector>
 
 using namespace std;
 
@@ -8,16 +13,44 @@ struct User {
     int userID;
 };
 
-void diplayUserMeny();
+void diplayUserMenu();
 
 int main()
 {
-    diplayUserMeny();
+    vector <User> users;
 
+    int selection = 0;
+    while(1) {
+        diplayUserMenu();
+        cin >> selection;
+
+        while(cin.fail()){ //Zabezpiecznie przed wpisaniem innych znakow niz liczby
+            cout << "Wpisz liczbe (1 - 3) : " << endl;
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(),'\n');
+            cin >> selection;
+        }
+        switch(selection) {
+        case 1:
+            //loggin();
+            break;
+        case 2:
+            //signIn(users);
+            break;
+        case 3:
+            exit(0);
+            break;
+        default:
+            cout<<"Nie ma takiej opcji!"<<endl;
+            Sleep(2000);
+            break;
+        }
+        system("cls");
+    }
     return 0;
 }
 
-void diplayUserMeny(){
+void diplayUserMenu(){
     cout << " --- User menu ---" << endl;
     cout << endl;
     cout << "1. Logowanie" << endl;
