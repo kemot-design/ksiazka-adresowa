215f73e0f49dbf4279f2d2ba814133c53e6c0d8c
Tomasz Budzyński <tomasz.budzynski.programista@gmail.com>
2020-05-06 12:58:47 +0200

    Add signing in feature

--- main.cpp
+++ main.cpp
@@ -14,10 +14,14 @@ struct User {
 };
 
 void diplayUserMenu();
+void addNewUser(vector <User> &users);
+void saveUsersToFile(const vector <User> &users);
+//void loadUsersFromFile(vector <User> &users);
 
 int main()
 {
     vector <User> users;
+    //loadUsersFromFile(users);
 
     int selection = 0;
     while(1) {
@@ -35,7 +39,8 @@ int main()
             //loggin();
             break;
         case 2:
-            //signIn(users);
+            addNewUser(users);
+            saveUsersToFile(users);
             break;
         case 3:
             exit(0);
@@ -57,3 +62,58 @@ void diplayUserMenu(){
     cout << "2. Rejestracja" << endl;
     cout << "3. Zamknij program" << endl;
 }
+
+void addNewUser(vector <User> &users) {
+    fstream loadFile("users.txt", ios::in);
+
+    if(!loadFile.is_open()) {
+        fstream loadFile("users.txt", ios::out);
+        loadFile.close();
+    }
+
+    system("cls");
+    string userName, userPw;
+
+    cout << "--- Rajestracja ---" << endl << endl;
+    cout << "Wpisz login: ";
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(),'\n');
+    cin >> userName;
+    cout << "Wpisz haslo: ";
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(),'\n');
+    cin >> userPw;
+
+    User newUser;
+    newUser.name = userName;
+    newUser.password = userPw;
+    if(users.empty()){
+        newUser.userID = 1;
+    }
+    else{
+        vector<User>::iterator itr = users.end();
+        itr--;
+        newUser.userID = (*itr).userID + 1;
+    }
+
+    users.push_back(newUser);
+    cout<<endl<<"Uzytkownik dodany"<<endl;
+    Sleep(2000);
+}
+
+void saveUsersToFile(const vector <User> &users){
+    fstream saveFile("users.txt", ios::out);
+    if(!saveFile.is_open()) {
+        cout<<"Blad otwarcia pliku do zapisu"<<endl;
+        return;
+    }
+
+    vector <User> :: const_iterator itr = users.begin();
+    for(itr ; itr != users.end() ; ++itr) {
+        saveFile << itr->userID << "|";
+        saveFile << itr->name << "|";
+        saveFile << itr->password << endl;
+    }
+    saveFile.close();
+}
+
